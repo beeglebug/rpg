0dcc9130bcd96373f1efb06e270bf8c283193bcd
beeglebug <updownleftright@gmail.com>
2015-02-09 21:50:58 +0000

    debug reticule

--- js/class/Debug.js
+++ js/class/Debug.js
@@ -6,13 +6,22 @@ Debug.init = function(stage) {
     
     this.mousePos = new PIXI.Text('0,0', { font: '10px Verdana', fill: '#333333' });
     this.mousePos.position.set(750,10);
-    
+
+    this.target = new PIXI.Sprite.fromImage('img/target.png');
+    this.target.anchor.set(0.454545,0.454545);
+
+    //this.container.addChild(this.target);
     this.container.addChild(this.mousePos);
 };
 
 Debug.setMousePos = function(pos) {
     
     this.mousePos.setText(pos.x + ',' + pos.y);
-    
 };
+
+Debug.setTarget = function(x, y) {
+
+    this.target.position.set(x, y);
+};
+
     
