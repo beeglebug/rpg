ac4294abe89b1996df962c764e6ad69e13b09fb2
beeglebug <updownleftright@gmail.com>
2015-03-07 13:26:06 +0000

    use gulp instead of npm script

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
+var project = require('./package.json');
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var wrap = require('gulp-wrap');
-var through = require('gulp-through');
-var changeCase = require('change-case');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var transform = require('vinyl-transform');
 
-var transform = through('transform', function(file, contents) {
+gulp.task('browserify', function() {
 
-    file.key = file.path.replace(file.base, '').replace('.json', '').split('\\').map(function(name) {
-        return changeCase.camelCase(name);
-    }).join('.');
-});
+    var browserified = transform(function(filename) {
+        var b = browserify(filename, { paths: ['./node_modules', './js/src', './js/lib'], debug : true });
+        return b.bundle();
+    });
 
-gulp.task('data', function() {
-    return gulp.src('data/**/*.json')
-        .pipe(transform())
-        .pipe(wrap('<%= file.key %> : <%= contents %>',{},{ parse : false }))
-        .pipe(concat('data.js'))
-        .pipe(wrap('var data = {\n<%= contents %>\n};'))
-        .pipe(gulp.dest('./build'));
+    return gulp.src('./js/index.js')
+        .pipe(browserified)
+        .pipe(rename(project.name + '.js'))
+        .pipe(gulp.dest('./build/'))
+        .pipe(uglify())
+        .pipe(rename(project.name + '.min.js'))
+        .pipe(gulp.dest('./build/'))
+    ;
 });
--- scripts/build.js
+++
@@ -1,7 +0,0 @@
-/* jshint node:true */
-'use strict';
-
-var browserify = require('browserify');
-var b = browserify('./js/index.js', { paths: ['./node_modules', './js/src', './js/lib'], debug : true });
-
-b.bundle().pipe(process.stdout);
